e8116c745f1a14ddad206775d05ec39510cc54bf
roquegabriel <perezsilva@roquegabriel.com>
2024-07-02 19:15:05 -0400

    Update to responsive design

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -16,12 +16,12 @@ const Reviews = () => {
                 <RiDoubleQuotesR className='text-5xl text-primary' />
             </div>
             <div className=' flex flex-col gap-8 items-center justify-center'>
-                <div className='flex items-center justify-center gap-12'>
-                    <img src="https://img.freepik.com/foto-gratis/hermosa-mujer-negocios-cabello-corto-oscuro-camisa-blanca-sosteniendo-carpeta-negra-manos-mientras-mira-alegremente-camara-sobre-fondo-azul_574295-3492.jpg" alt="Business woman" className='h-14 w-14 rounded-full object-cover' />
-                    <img src="https://img.freepik.com/foto-gratis/retrato-exitosa-mujer-negocios-sonriente-mirando-camara-sentada-restaurante-mujer-negocios-peinado-elegante-viste-traje-elegante-reunion-negocios-aspecto-atractivo_8353-12611.jpg" alt="" className='h-16 w-16 rounded-full object-cover' />
-                    <img src="https://img.freepik.com/foto-gratis/hombre-negocios-confiado-que-sostiene-tableta-digital-sonriente-pie_1258-26615.jpg" alt="Bussinessman" className='h-20 w-20 rounded-full object-cover ring-4 ring-primary p-1 bg-white' />
-                    <img src="https://img.freepik.com/foto-gratis/retrato-mujer-feliz-gerente-tocar-cara_1262-3808.jpg" alt="" className='h-16 w-16 rounded-full object-cover' />
-                    <img src="https://img.freepik.com/foto-gratis/empresario-sosteniendo-portatil-apuntando-arriba-sobre-fondo-blanco_114579-28447.jpg" alt="" className='h-14 w-14 rounded-full object-cover' />
+                <div className='flex items-center justify-center gap-8 md:gap-12'>
+                    <img src="https://img.freepik.com/foto-gratis/hermosa-mujer-negocios-cabello-corto-oscuro-camisa-blanca-sosteniendo-carpeta-negra-manos-mientras-mira-alegremente-camara-sobre-fondo-azul_574295-3492.jpg" alt="Business woman" className='h-8 w-8 md:h-14 md:w-14 rounded-full object-cover' />
+                    <img src="https://img.freepik.com/foto-gratis/retrato-exitosa-mujer-negocios-sonriente-mirando-camara-sentada-restaurante-mujer-negocios-peinado-elegante-viste-traje-elegante-reunion-negocios-aspecto-atractivo_8353-12611.jpg" alt="" className='md:h-16 md:w-16 w-10 h-10 rounded-full object-cover' />
+                    <img src="https://img.freepik.com/foto-gratis/hombre-negocios-confiado-que-sostiene-tableta-digital-sonriente-pie_1258-26615.jpg" alt="Bussinessman" className='md:h-20 md:w-20 w-12 h-12 rounded-full object-cover ring-4 ring-primary p-1 bg-white' />
+                    <img src="https://img.freepik.com/foto-gratis/retrato-mujer-feliz-gerente-tocar-cara_1262-3808.jpg" alt="" className='md:h-16 md:w-16 w-10 h-10 rounded-full object-cover' />
+                    <img src="https://img.freepik.com/foto-gratis/empresario-sosteniendo-portatil-apuntando-arriba-sobre-fondo-blanco_114579-28447.jpg" alt="" className='h-8 w-8 md:h-14 md:w-14 rounded-full object-cover' />
                 </div>
                 <div className='text-center'>
                     <h3 className='text-[24px] font-bold'>Ricky Ricon</h3>
