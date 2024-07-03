ec757428d0427cf71af8da83dfcc49cacadeb3b8
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-03 01:12:49 -0400

    Fix image path

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,9 @@
+import firstWork from '../assets/images/work1.png'
+import secondWork from '../assets/images/work2.png'
+import thirdWork from '../assets/images/work3.png'
+import fourthWork from '../assets/images/work4.png'
+import fifthWork from '../assets/images/work5.png'
+
 import React from 'react'
 
 const Works = () => {
@@ -10,7 +16,7 @@ const Works = () => {
             {/* Works */}
             <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
                 <div className='flex flex-col gap-2'>
-                    <img src="src/assets/images/work1.png" alt="Work one" className='w-full h-[600px] object-cover rounded-3xl mb-4' />
+                    <img src={firstWork} alt="Work one" className='w-full h-[600px] object-cover rounded-3xl mb-4' />
                     <p className='text-gray-500'>App design - June 20, 2023</p>
                     <h3 className='text-2xl font-bold'>App Redesign</h3>
                     <p className="text-gray-500">
@@ -19,22 +25,22 @@ const Works = () => {
                 </div>
                 <div className='grid grid-cols-1 xl:grid-cols-2 gap-8'>
                     <div>
-                        <img src="src/assets/images/work2.png" alt="Second work" className='w-full rounded-3xl h-56 object-cover' />
+                        <img src={secondWork} alt="Second work" className='w-full rounded-3xl h-56 object-cover' />
                         <p className='text-gray-500'>App design - June 20, 2023</p>
                         <h3 className='text-2xl font-bold'>Redesign channel website landing page</h3>
                     </div>
                     <div>
-                        <img src="src/assets/images/work3.png" alt="Third work" className='w-full rounded-3xl h-56 object-cover' />
+                        <img src={thirdWork} alt="Third work" className='w-full rounded-3xl h-56 object-cover' />
                         <p className='text-gray-500'>App design - June 20, 2023</p>
                         <h3 className='text-2xl font-bold'>New locator app for a new company</h3>
                     </div>
                     <div>
-                        <img src="src/assets/images/work4.png" alt="Fourth work" className='w-full rounded-3xl h-56 object-cover' />
+                        <img src={fourthWork} alt="Fourth work" className='w-full rounded-3xl h-56 object-cover' />
                         <p className='text-gray-500'>App design - June 20, 2023</p>
                         <h3 className='text-2xl font-bold'>Rental rooms web app platform</h3>
                     </div>
                     <div>
-                        <img src="src/assets/images/work5.png" alt="Fifth work" className='w-full rounded-3xl h-56 object-cover' />
+                        <img src={fifthWork} alt="Fifth work" className='w-full rounded-3xl h-56 object-cover' />
                         <p className='text-gray-500'>App design - June 20, 2023</p>
                         <h3 className='text-2xl font-bold'>Calendar app for big SASS Company</h3>
                     </div>
