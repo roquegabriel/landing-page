4e65274405e38962975f051115daf5dfb25664de
roquegabriel <perezsilva@roquegabriel.com>
2024-07-02 22:20:35 -0400

    Update custome style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
     extend: {
       colors:{
         'primary':'#706FE5',
-        'secondary':'#EAEAFC'
+        'secondary':'#EAEAFC',
+        'footer':'#2E3E5C'
       },
     },
   },
